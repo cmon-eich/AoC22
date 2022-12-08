2939c5fd85b29e23123d97652a4acc7f7348d1de
cmon-eich <cmon.eich@gmail.com>
2022-12-08 17:48:33 +0100

    6 (... R is ok)

---
+++ 6/6.2.r
@@ -0,0 +1,25 @@
+#!/usr/bin/env Rscript
+library(readr)
+data = read_lines("input.txt")
+# data = c("abasdfasdf")
+let = strsplit(data[1], "")[[1]]
+code <- let[1:14]
+pos = 1
+for (l in let) {
+	code = append(code[2:length(code)], l)
+	tmp <- table(code)
+	code_is_right = TRUE
+	print(code)
+	for (c in code) {
+		if (tmp[c]>1) {
+			code_is_right = FALSE
+		}
+	}
+	if (code_is_right) {
+		print(length(let))
+		print(pos)
+		stop()
+	}
+	pos = pos + 1
+}
+
