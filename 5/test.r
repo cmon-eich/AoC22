15c93863ed78a571eeff11df6ce18a9211fcfe2d
Simon Eichinger <cmon.eich@gmail.com>
2022-12-08 14:40:19 +0100

    5 ...

--- 5/test.r
+++ 5/test.r
@@ -20,18 +20,25 @@ for (d in data) {
 	} else if (process_plan) {
 		plan <- append(plan, d)
 	} else {
-		d = substr(d, 6, length(d))
+		d = substr(d, 6, nchar(d))
 		d = strsplit(d, " from ")[[1]]
 		count = strtoi(d[1])
-		d = strsplit(d[2], " to ")
+		d = strsplit(d[2], " to ")[[1]]
 		from = strtoi(d[1])
 		to = strtoi(d[2])
-		if (count >= length(from)) {
+		if (count >= length(containers[[from]])) {
 			containers[[to]] = append(containers[[to]], containers[[from]])
-			containers[[from]] <- list()
+			containers[[from]] <- c() #list()
 		} else {
-			temp = containers[[from]][length(containers[[from]]-count):lenght(containers[[from]])]
+			temp = containers[[from]][length(containers[[from]])-count+1:length(containers[[from]])]
+			print(length(containers[[from]])-count)
+			print(containers[[from]])
+			containers[[from]] = containers[[from]][1:(length(containers[[from]])-count)]
+			containers[[to]] = append(containers[[to]], temp)
 		}
 	}
 }
 print(containers)
+# for (i in 1:length(containers)) {
+# 	print(container[[i]][length(containers[[i]])])
+# }
